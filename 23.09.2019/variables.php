b6ad87b0087fe0779e0ce528c0f3e49b82e0b59a
Malle Alaru <malle.alaru@khk.ee>
2019-09-23 12:26:37 +0300

    Muutujate ja andmetüüpide põhikasutus

--- 23.09.2019/variables.php
+++ 23.09.2019/variables.php
@@ -1,3 +1,17 @@
 <?php
 //muutujate defineerimine + väärtuse omistamine
 // $muutujaNimi = väärtus;
+// andmetüübid php-s:
+// sõned jutumärkidesse, sobivad nii kahe kui ühekordsed jutumärgid;
+// numbrid: täisarvud (ka negatiivsed), reaalarvud (1.5, 2.85)
+
+$eesNimi = "Malle";
+$pereNimi = "Alaru";
+$yksus = "ikt osakond";
+$email = "malle.alaru@khk.ee";
+
+//muutujate kasutamine - selleks tuleb neid kutsuda
+
+echo $eesNimi . " " . $pereNimi . "<br>";
+echo $yksus . "<br>";
+echo $email . "<br>";
