4bd1d958d796ad21845b5e8c3a60b1fc2bd18c27
Malle Alaru <malle.alaru@khk.ee>
2019-09-27 15:08:05 +0300

    Kordamine get meetod

--- 27.09.2019/circle_do.php
+++ 27.09.2019/circle_do.php
@@ -1,7 +1,19 @@
 <?php
-//circle_do
+//circle_do.php
 $circleRadius = $_GET['circle-radius'];
 
-// sisu kontroll, mis on sisu, mis tyypi jms
+// sisu kontroll, mis on sisu, mis tyypi jms var_dump - midagi ei õnnestunud, browseris erines õp omast
+//var_dump($circleRadius);
+//$sum = $circleRadius + $circleRadius;
+//var_dump($sum);
 
-var_dump($circleRadius);
+//arvutused
+
+$circleArea = 3.14 * $circleRadius * $circleRadius;
+$circleCircumference = 3.14 * 2 * $circleRadius;
+// print
+
+echo 'Raadius: ' . $circleRadius . '<br>';
+echo '<hr>';
+echo 'Ringi pindala = ' . $circleArea . '<br>';
+echo 'Ringi ümbermõõt = ' . $circleCircumference . '<br>';
--- 27.09.2019/circle_input.php
+++ 27.09.2019/circle_input.php
@@ -2,8 +2,9 @@
 // circle_input.php
 ?>
 
-<form action="circle_do.php" method="get">
+<form action="circle_do.php" method="get"> //siin on kasutatud geti meetodit
     <label for="radius">Sisesta ringi raadius: </label>
-    <input type="number" id="radius" name="circle-radius">
+    <input type="number" id="radius" name="circle-radius"> //name on vormi nimi, mis on pärast aadressiribal peale
+    ?märki
 </form>
 
