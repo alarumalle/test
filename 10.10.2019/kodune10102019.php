d5bb5b11e9716db620316b666b3bf758ee0789fd
Malle Alaru <malle.alaru@khk.ee>
2019-10-10 12:06:09 +0300

    Metshein - aritmeetika - suurendamine 1 võrra

---
+++ 10.10.2019/kodune10102019.php
@@ -0,0 +1,9 @@
+<?php
+//kodune töö, Ülesanne 2, 10.10.2019
+//F=(9/5)*C+32
+
+$kraad_C = 100;
+
+$teisendus = (9 / 5) * $kraad_C + 32;
+
+echo '100 kraadi Celsiuse skaalal on ' . $teisendus . ' Farenheiti skaalal.';
