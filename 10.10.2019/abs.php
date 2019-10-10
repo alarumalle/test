5ec54b5aa4709700ee98a8ba2136c199363f48ed
Malle Alaru <malle.alaru@khk.ee>
2019-10-10 12:39:55 +0300

    Metshein - aritmeetika - Absoluutväärtus

---
+++ 10.10.2019/abs.php
@@ -0,0 +1,9 @@
+<?php
+
+$number = 15;
+if ($number < 0) {
+    $abs = $number * -1;
+} else {
+    $abs = $number;
+}
+echo $number . ' absoluutväärtus on ' . $abs;
---
+++ 10.10.2019/logical_operators.php
@@ -0,0 +1,24 @@
+<?php
+
+//defineerime muutujad
+
+$x = 2;
+$y = 3;
+// Logical operators
+
+var_dump($x == $y);
+echo '<hr>';
+
+var_dump($x != $y);
+echo '<hr>';
+
+var_dump($x < $y);
+
+var_dump($x > $y);
+echo '<hr>';
+
+var_dump($x >= $y);
+echo '<hr>';
+
+var_dump($x <= $y);
+echo '<hr>';
