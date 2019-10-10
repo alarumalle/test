f675632927e51569663d995c99804abc58791cc8
Malle Alaru <malle.alaru@khk.ee>
2019-10-10 13:04:23 +0300

    Metshein - Nulliga jagamine

---
+++ 9.10.2019/divide_by_zero_form.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<form action="">
+    <div class="input-group">
+        <label for="numberOne">Sisesta 1. arv:</label>
+        <input type="text" id="numberOne" name="numberOne">
+    </div>
+    <br>
+    <div class="input-group">
+        <label for="numberTwo">Sisesta 2. arv:</label>
+        <input type="text" id="numberTwo" name="numberTwo">
+    </div>
+</form>
