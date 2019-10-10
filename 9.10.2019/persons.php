612a810a271b3c789a10a0274e9938880da9e112
Malle Alaru <malle.alaru@khk.ee>
2019-10-10 12:57:58 +0300

    Metshein - Seostatud IF ELSE lause

--- 9.10.2019/persons.php
+++ 9.10.2019/persons.php
@@ -1,13 +1,14 @@
 <?php
 
-$age = 5;
+$age = -75;
 echo 'Vanus = ' . $age . '<br>';
 if ($age > 0 and $age < 18) {
     echo 'Oled laps.';
-}
-if ($age > 17 and $age < 66) {
+    //else if kui ta leiab vastuse esimesest if lausest, siis ta ei lähe edasi, et mitte süsteemi koormata.
+} else if ($age > 17 and $age < 66) {
     echo 'Oled täiskasvanu.<br>';
-}
-if ($age > 65) {
+} else if ($age > 65) {
     echo 'Oled seenior.<br>';
+} else {
+    echo 'Midagi on valesti.';
 }
