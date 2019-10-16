8b7376013db731b4ed084c57bd0440ec28cdb732
Malle Alaru <malle.alaru@khk.ee>
2019-10-16 16:27:52 +0300

    pi arvutamine ei tööta

---
+++ 16.10.2019/pi_value.php
@@ -0,0 +1,23 @@
+<?php
+
+//EI TÖÖTA
+//niikaua kui count on väiksem või võrdne maksimumist seni me teeme tehet
+
+
+$maxValue = 1000:
+$count = 1;
+$sum = 0;
+
+while ($count <= $maxValue) {
+    $elem = 1 / (2 * $count - 1);
+    if ($count % 2 != 0) {
+        $sum = $sum + $elem;
+    } else {
+        $sum = $sum - $elem;
+    }
+    $count++;
+}
+
+echo 'pi = ' . ($sum * 4) . '<br>';
+echo 'pi = ' . pi();
+
