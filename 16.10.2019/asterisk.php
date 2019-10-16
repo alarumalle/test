501801939456a0f394094bf9c5a31220becc3ba0
Malle Alaru <malle.alaru@khk.ee>
2019-10-16 15:47:02 +0300

    tärnid for tsükklis

---
+++ 16.10.2019/asterisk.php
@@ -0,0 +1,13 @@
+<?php
+
+//for tsükkel tjm alg, tjm lopp ja suurendaja tjm++ või tjm--
+
+for ($count = 1; $count <= 25; $count++) {
+    echo $count;
+    if ($count < 10) {
+        echo '$nbsp; $nbsp; $nbsp;';
+    } else {
+        echo '$nbsp';
+    }
+    echo '*<br>';
+}
