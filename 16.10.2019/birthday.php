182b391851db1ce870a85a825577dffb4092dc82
Malle Alaru <malle.alaru@khk.ee>
2019-10-16 14:52:32 +0300

    juubel voi mitte

---
+++ 16.10.2019/birthday.php
@@ -0,0 +1,15 @@
+<?php
+
+$birthYear = $_GET['birthYear'];
+
+if (strlen($birthYear) > 0) {
+    $age = 2019 - $birthYear;
+    if (($age % 5) == 0) {
+        echo 'Sul on juubel, oled ' . $age . ' aastat vana';
+    } else {
+        echo 'Sul on tavasünnipäev, sa oled' . $age . ' aastat vana';
+    }
+}
+
+//http://alarumalle.ikt.khk.ee/PA/16.10.2019/birthday.php?birthYear=2000
+// aadressiriba lõppu ? siis birthYear= ja siis mingi synniaasta
