a4caacd1594e103782cf9a4eae855d16a40660cd
Malle Alaru <malle.alaru@khk.ee>
2019-10-22 17:00:23 +0300

    table rows and columns

--- 22.10.2019/table.php
+++ 22.10.2019/table.php
@@ -3,4 +3,11 @@
 function tableRow($colNumber)
 {
     echo '<tr>';
+    for ($col = 1; $col < $colNumber; $col++) {
+        echo '<td style: background: red;' >;
+        echo 'cell';
+        echo '</td>';
+    }
+
+    echo '</tr>';
 }
--- Ylesanne_7/Kolmnurk_tulemus.php
+++ Ylesanne_7/Kolmnurk_tulemus.php
@@ -2,17 +2,32 @@
 
 $kylga = $_GET["kylg1"];
 $kylgb = $_GET["kylg2"];
+function kontroll($vaartus)
+{
+    if (!is_numeric($vaartus)) {
+        echo 'külg ' . $vaartus . ' peab olema number<br>';
+        $result = false;
+    } else if ($vaartus == '' or $vaartus == 0 or $vaartus < 0) {
+        echo 'Sisesta kolmnurga kylje väärtus, mis on positiivne arv<br>';
+        $result = false;
+    } else {
+        $result = true;
+    }
+    return $result;
+}
+
 function kolmnurk($kylga, $kylgb)
 {
     return round(sqrt(pow($kylga, 2) + pow($kylgb, 2)), 2);
 }
 
-if ($kylga == '' or $kylgb == '' or $kylga == 0 or $kylgb == 0 or $kylga < 0 or $kylgb < 0) {
-    echo 'Sisesta kaks kolmnurga kylge, mis on positiivsed arvud';
-} else {
+
+if (kontroll($kylga) or kontroll($kylgb)) {
     echo 'Kolmnurga kolmas kylg on: ', kolmnurk($kylga, $kylgb), ' cm';
+
+} else {
+    echo 'Kontrolli vaartused!';
 }
 echo "<br>";
-
 ?>
 
