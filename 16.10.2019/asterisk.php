78f7c60dc561fc38d65aa47e411506453f4b5191
alarumalle <55531501+alarumalle@users.noreply.github.com>
2019-10-23 15:20:31 +0300

    kood parandatud

--- 16.10.2019/asterisk.php
+++ 16.10.2019/asterisk.php
@@ -5,27 +5,9 @@
 for ($count = 1; $count <= 25; $count++) {
     echo $count;
     if ($count < 10) {
-        echo '$nbsp; $nbsp; $nbsp;';
+        echo '&nbsp; &nbsp;';
     } else {
-        echo '$nbsp';
-    }
-    echo '*<br>';
-}
-
-
-/**
- * file name: asteriks_column.php;
- * autor: anna.karutina;
- * date: 16.10.2019;
- */
-// for(tjm = alg; tjm <= lopp; tjm++)
-// for(tjm = lopp; tjm > alg; tjm--)
-for ($count = 1; $count <= 25; $count++) {
-    echo $count;
-    if ($count < 10) {
-        echo '&nbsp;&nbsp;&nbsp;';
-    } else {
-        echo '&nbsp;';
+        echo '&nbsp';
     }
     echo '*<br>';
 }
