fbd22fd62e9cb627084d87d2038d626356db058b
alarumalle <55531501+alarumalle@users.noreply.github.com>
2019-10-23 16:00:05 +0300

    Add files via upload

---
+++ Ylesanne_7/Kolmnurk_sisestusV2.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Funktsioonid - kolmnurk</title>
+</head>
+<body>
+    <h1>Ylesanne 7, kolmnurk</h1>
+    <form action="Kolmnurk_tulemus.php" method="get">
+        Kolmnurga kylg1 <input type="text" name="kylg1"><br>
+        <br>
+        Kolmnurga kylg 2 <input type="text" name="kylg2"><br>
+        <br>
+        <input type="submit" value="Arvuta kolmas kylg"><br>
+        <br>
+        <hr>
+    </form>
+</body>
+</html>
---
+++ Ylesanne_7/Kolmnurk_tulemusV1.php
@@ -0,0 +1,17 @@
+<?php
+
+$kylga = $_GET["kylg1"];
+$kylgb = $_GET["kylg2"];
+function kolmnurk($kylga, $kylgb){
+	return round(sqrt(pow($kylga,2) + pow($kylgb,2)),2);
+}
+
+if(!is_numeric($kylga) or !is_numeric($kylgb) or $kylga == 0 or $kylgb == 0 or $kylga < 0 or $kylgb < 0){
+    echo 'Sisesta kaks kolmnurga kylge, mis on positiivsed arvud';
+} else {
+    echo 'Kolmnurga kolmas kylg on: ', kolmnurk($kylga, $kylgb), ' cm';
+}
+echo "<br>";
+
+?>
+
