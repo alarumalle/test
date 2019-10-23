e5dbcdb73365a36a5eb4f2ff1776ded4dbe9f779
alarumalle <55531501+alarumalle@users.noreply.github.com>
2019-10-23 13:54:36 +0300

    täiendatud kood

--- 22.10.2019/prime_number.php
+++ 22.10.2019/prime_number.php
@@ -1,4 +1,37 @@
 <?php
+function isPrime($number){
+  if($number < 2) {
+    $result = 'this number is not valid';
+  } else {
+    $divider = 2;
+    while ($number % $divider != 0) {
+      $divider++;
+    }
+    if($number == $divider) {
+      $result = true;
+    } else {
+      $result = false;
+    }
+  }
+  return $result;
+}
+function description($number) {
+  echo $number;
+  if(isPrime($number) === true){
+    echo ' is prime';
+  } else if(isPrime($number) === false){
+    echo ' is not prime';
+  } else {
+    echo ' - '.isPrime($number);
+  }
+  echo '<br>';
+}
+description(rand(0,99));
+
+
+
+//ilma kirjelduseta
+/*<?php
 
 function isPrime($number)
 {
@@ -20,5 +53,6 @@ function isPrime($number)
 isPrime(rand(0, 99));
 isPrime(2);
 ?>
+*/
 
 
